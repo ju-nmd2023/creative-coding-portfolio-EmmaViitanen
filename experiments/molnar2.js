6851bc4d8487b940cb70f00ca586f93bee2de8b9
Emma Viitanen <viem23ni@student.ju.se>
2025-09-12 11:05:27 +0200

    roses

--- experiments/molnar2.js
+++ experiments/molnar2.js
@@ -1,7 +1,6 @@
 //This code was inspired by Garrit's molnar.js and modified by me
 function setup() {
-  createCanvas(600, 600);
-  frameRate(2);
+  createCanvas(1000, 900);
 }
 
 const size = 100;
@@ -11,38 +10,34 @@ function getRandomValue(pos, variance) {
   return pos + map(Math.random(), 0, 1, -variance, variance);
 }
 
-function randomColor() {
-  let r = random(180, 255);
-  let g = random(180, 255);
-  let b = random(180, 255);
-  return color(r, g, b, 300);
-}
-
 function drawLayers(x, y, size, layers) {
   // const half = size / 2;
-  const variance = size / 20;
+  const variance = size / 15;
   noFill();
+  stroke(255, 3, 62);
+  strokeWeight(random(0.5, 2));
   // rectMode(CENTER);
   for (let i = 0; i < layers; i++) {
     if (Math.random() > 0.8) {
       continue;
     }
-    stroke(randomColor());
-    strokeWeight(5);
-    const s = (size / layers) * i;
-    const half = s / 2;
+
+    // The following 9 lines of code was adapted from https://chatgpt.com/share/68c3e0df-e860-800b-af9c-fecc33c1663a Accessed: 2025-09-04
+    const r = (size / 2 / layers) * i;
+    // const half = s / 2;
     beginShape();
-    vertex(getRandomValue(x - half, variance), getRandomValue(y - half, variance));
-    vertex(getRandomValue(x + half, variance), getRandomValue(y - half, variance));
-    vertex(getRandomValue(x + half, variance), getRandomValue(y + half, variance));
-    vertex(getRandomValue(x - half, variance), getRandomValue(y + half, variance));
+    for (let a = 0; a < TWO_PI; a += PI / 10) {
+      const px = getRandomValue(x + cos(a) * r, variance);
+      const py = getRandomValue(y + sin(a) * r, variance);
+      vertex(px, py);
+    }
     endShape(CLOSE);
     // rect(x - half, y - half, s, s);
   }
 }
 
 function draw() {
-  background(255);
+  background(71, 10, 10);
 
   // drawLayers(100, 100, size, layers);
   for (let y = 0; y < 10; y++) {
@@ -51,5 +46,5 @@ function draw() {
     }
   }
 
-  //   noLoop();
+  noLoop();
 }
